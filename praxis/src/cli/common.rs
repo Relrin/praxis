82051cd2fa34930f6b7cbb248a47dd373632616b
Relrin <relrin78@gmail.com>
2026-03-16 23:35:08 +0100

    Added deps for new supported languages

--- praxis/src/cli/common.rs
+++ praxis/src/cli/common.rs
@@ -11,6 +11,9 @@ pub enum OutputFormat {
 /// Creates a [`PluginRegistry`] with all built-in language analyzers.
 pub fn default_plugin_registry() -> PluginRegistry {
     let mut plugins = PluginRegistry::new();
+    plugins.register(Box::new(praxis_lang_angelscript::AngelScriptAnalyzer::new()));
+    plugins.register(Box::new(praxis_lang_cpp::CppAnalyzer::new()));
+    plugins.register(Box::new(praxis_lang_elixir::ElixirAnalyzer::new()));
     plugins.register(Box::new(praxis_lang_rust::RustAnalyzer::new()));
     plugins.register(Box::new(praxis_lang_go::GoAnalyzer::new()));
     plugins.register(Box::new(praxis_lang_ts::TypeScriptAnalyzer::new()));
